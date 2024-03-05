9b31175225ed1162d3f6e557e178ce5ff9cee00a
linnkhant07 <lkty07@gmail.com>
2024-03-04 18:26:29 -0800

    navbar done

---
+++ script.js
@@ -0,0 +1,8 @@
+//click hamburger to collapse
+function toggleMenu(){
+    const icon = document.querySelector(".hamburger-icon")
+    const menu = document.querySelector(".hamburger-links")
+
+    icon.classList.toggle("open");
+    menu.classList.toggle("open");
+}
