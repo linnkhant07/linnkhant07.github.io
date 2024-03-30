0dded54bacdcd328dca7c799983b3bcd4031100e
linnkhant07 <lkty07@gmail.com>
2024-03-29 20:47:25 -0700

    fixed z-index issue

--- assets/js/port_folio.js
+++ assets/js/port_folio.js
@@ -182,3 +182,4 @@ function scrollUp() {
     if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
   }
   window.addEventListener('scroll', scrollUp)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,30 @@
 //click hamburger to collapse
 function toggleMenu(){
+    
     const icon = document.querySelector(".hamburger-icon")
     const menu = document.querySelector(".hamburger-links")
 
     icon.classList.toggle("open");
     menu.classList.toggle("open");
+}
+
+/*=============== SHOW SIDEBAR ===============*/
+const navMenu = document.getElementById('sidebar'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== SIDEBAR SHOW =====*/
+/* Validate If Constant Exists */
+if(navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add('show-sidebar')
+    })
+}
+
+/*===== SIDEBAR HIDDEN =====*/
+/* Validate If Constant Exists */
+if(navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove('show-sidebar')
+    })
 }
